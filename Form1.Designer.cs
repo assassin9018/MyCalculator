5bd24df6d8caf519e570b082870d3281cb884c09
assassin9018 <qwertypascal@mail.ru>
2018-06-25 08:41:23 +0300

    0

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -77,7 +77,6 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(404, 21);
             this.textBox1.TabIndex = 5;
-            this.textBox1.Text = "0.1+1.1";
             this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // MultyParamsFunc

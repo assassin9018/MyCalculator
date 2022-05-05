6a5eccd3d5cd72bae3de719ed9dbc7fa72f137f5
beskaravaev.a <34675794+assassin9018@users.noreply.github.com>
2022-05-05 12:27:44 +0300

    Benchmark added. Tests updated and some new tests added.

--- CalcBenchConsole/CalcBenchmarks.cs
+++ CalcBenchConsole/CalcBenchmarks.cs
@@ -9,7 +9,8 @@ namespace CalcBenchConsole;
 [RPlotExporter]
 public class CalcBenchmarks
 {
-    [Params("2*-2+2--2", "int((((13-2)))*100*sin(45)^(1/2)+40)", "2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2")]
+    [Params("2*-2+2--2", "intg((((13-2)))*100*sin(45)^(1/2)+40)", "2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2",
+        "intg(abs(-50*2)*sin(45)*cos(30)+tan(70))+rnd(exp(7))+ln(32)+sqrt(sqr(log(100)))")]
     public string N;
 
     [GlobalSetup]
--- MyCalcTests/CalcComparationTest.cs
+++ MyCalcTests/CalcComparationTest.cs
@@ -1,9 +1,10 @@
 using Calculation;
 using NUnit.Framework;
 using System;
-using System.Collections.Generic;
+using System.Linq;
 
 namespace MyCalcTests;
+
 public class Tests
 {
     [SetUp]
@@ -14,39 +15,58 @@ public class Tests
     [Test]
     public void MulTest()
     {
-        int round = 0;
         string exprassion = "2*2";
-        CompareCalcs(round, exprassion);
+        CompareCalcs(exprassion);
+        Assert.Pass();
     }
 
     [Test]
     public void BracketsTest()
-    { 
-        int round = 0;
+    {
         string exprassion = "(2+2)*2";
-        CompareCalcs(round, exprassion);
+        CompareCalcs(exprassion);
+        Assert.Pass();
     }
 
     [Test]
     public void PriorityTest()
     {
+        string exprassion = "log(100)^2*2+2";
+        CompareCalcs(exprassion);
+        Assert.Pass();
     }
 
     [Test]
     public void OneArgFuncTest()
-    { 
-        int round = 0;
-        string exprassion = "int(16.0) + abs(-4)";
-        CompareCalcs(round, exprassion);
+    {
+        int round = 5;
+        var names = OneArgFunctionTypeExtensions.GetNames();
+        foreach(string func in names)
+            CompareCalcs(func + "(1)", round);
+        Assert.Pass();
     }
 
-    private static void CompareCalcs(int round, string exprassion)
+    [Test]
+    public void IgnoreCaseTest()
+    {
+        int round = 5;
+        var names = OneArgFunctionTypeExtensions.GetNames();
+
+        foreach(string func in names.Select(x=>x.ToUpper()))
+            CompareCalcs(func + "(1)", round);
+
+        foreach(string func in names.Select(x => x.ToLower()))
+            CompareCalcs(func + "(1)", round);
+
+        Assert.Pass();
+    }
+
+    private static void CompareCalcs(string exprassion, int round = 0)
     {
         SmartCalculator smartCalc = new(Array.Empty<string>());
         LegacyCalculator legacyCalc = new(round);
         double legacyResult = legacyCalc.Handle(exprassion);
         double smartResult = smartCalc.Execute(exprassion, round, new());
         Assert.AreEqual(legacyResult, smartResult);
-        Assert.Pass();
     }
 }

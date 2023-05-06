e73d4ccd32e50f78fc5b043bdf64d536bac3b2a4
Beskaravaev Andrey Vladimirovich <34675794+assassin9018@users.noreply.github.com>
2023-05-06 12:04:14 +0300

    update benchmark

--- CalcBenchConsole/CalcBenchmarks.cs
+++ CalcBenchConsole/CalcBenchmarks.cs
@@ -6,15 +6,21 @@ using System.Text;
 namespace CalcBenchConsole;
 
 [MemoryDiagnoser]
-[SimpleJob(RuntimeMoniker.Net60, baseline: true)]
+[SimpleJob(RuntimeMoniker.Net70, baseline: true)]
 [RPlotExporter]
 public class CalcBenchmarks
 {
-    [Params("2*-2+2--2", 
-        "intg((((13-2)))*100*sin(45)^(1/2)+40)", 
-        "2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2",
+    [Params("2*-2+2-------------------2",
+        "intg(((((((((((((13-2))))))))))))*100*sin(45)^(1/2/2/2/2/2/2)+40)",
+        "2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2+2",
+        "intg(abs(-50*2)*sin(45)*cos(30)+tan(70))+rnd(exp(7))+ln(32)+sqrt(sqr(log(100)))+" +
+        "intg(abs(-50*2)*sin(45)*cos(30)+tan(70))+rnd(exp(7))+ln(32)+sqrt(sqr(log(100)))+" +
+        "intg(abs(-50*2)*sin(45)*cos(30)+tan(70))+rnd(exp(7))+ln(32)+sqrt(sqr(log(100)))+" +
+        "intg(abs(-50*2)*sin(45)*cos(30)+tan(70))+rnd(exp(7))+ln(32)+sqrt(sqr(log(100)))+" +
+        "intg(abs(-50*2)*sin(45)*cos(30)+tan(70))+rnd(exp(7))+ln(32)+sqrt(sqr(log(100)))+" +
         "intg(abs(-50*2)*sin(45)*cos(30)+tan(70))+rnd(exp(7))+ln(32)+sqrt(sqr(log(100)))")]
     public string N;
+
     private readonly ICalculator _legacy = new LegacyCalculator(0);
     private readonly ICalculator _smart = new SmartCalculator(Array.Empty<string>());
 
@@ -24,10 +30,10 @@ public class CalcBenchmarks
     }
 
     [Benchmark(Baseline = true)]
-    public double RunLegacy() 
+    public double RunLegacy()
         => _legacy.Execute(N);
 
     [Benchmark]
-    public double RunSmart() 
+    public double RunSmart()
         => _smart.Execute(N);
 }
